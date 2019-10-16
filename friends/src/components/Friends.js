c416c79649ddc708cd400c3eef888f9a5e2236b2
Adam Payne <ajpayne238@gmail.com>
2019-10-15 22:52:56 -0400

    Rendering friend objects inside private route

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,26 +1,53 @@
 import React, { useState, useEffect } from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-const Friends = (props) => {
+const Friends = () => {
     const [ friends, setFriends ] = useState([])
 
     useEffect(() => {
         axiosWithAuth()
         .get("/api/friends")
-        .then(response => setFriends(console.log("Get Response", response.data), response.data))
+        .then(response => setFriends(response.data))
         .catch(error => console.log(console.log("Get Error", error), error))
     }, [])
 
     return(
         <div>
-             <h1>Friends</h1>
-             {/* {friends.map(friendObj => {
-                 <div key={friendObj.id}>
-                     <p>{friendObj.name}</p>
-                 </div>
-             })} */}
+             {friends.map(friendObj => {
+                 return <div key={friendObj.id}>
+                            <p>{friendObj.name}</p>
+                            <p>{friendObj.age}</p>
+                            <p>{friendObj.email}</p>
+                        </div>
+             })}
         </div>
     )
 }
 
 export default Friends;
+
+// const MyFriends = () => {
+//   const [friends, setFriends] = useState([])
+
+//   useEffect(() => {
+//     axiosWithAuth()
+//     .get("/friends")
+//     .then(res => setFriends(res.data))
+//     .catch(err => console.error(err))
+//   })
+
+//   return(
+//     <>
+//       <AddFriend />
+//       <h1>MY FRIENDS!</h1>
+//       {friends.map(friend => (
+//         <div key={friend.id}>
+//           <Friend banana={friend} />
+//         </div>
+//       ))}
+//     </>
+//   )
+
+// }
+
+// export default MyFriends; 
